1f2760b684ac09672680a9aff27ba0cee92e393f
Coyot Willy <coyotwilly@gmail.com>
2024-10-24 17:39:27 +0200

    Project init
    Project setup

---
+++ src/main/java/com/coyotwilly/ctrain/CTrainApplication.java
@@ -0,0 +1,13 @@
+package com.coyotwilly.ctrain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CTrainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CTrainApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/coyotwilly/ctrain/CTrainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coyotwilly.ctrain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CTrainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
